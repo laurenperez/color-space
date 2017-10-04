ccf95ff3cbe78dadc5eeb926d083f1a120993ee8
laurenperez <laurenashleymarks@gmail.com>
2017-10-04 14:42:51 -0700

    added db calls to routes to show images

--- controllers/spaces.js
+++ controllers/spaces.js
@@ -15,23 +15,15 @@ var images = [];
 
 /////// THIS IS DONE DONT TOUCH IT ///////
 router.get('/', isLoggedIn, function(req, res) {
-  res.render('spaces/profile');
+  db.space.findAll({
+    where: {
+      userId: req.user.id
+    },
+  }).then(function(spaces){
+    res.render('spaces/profile', {spaces: spaces});
+  });
 });
 
-
-router.get('/:id', isLoggedIn, function(req, res) {
-  //get room name by id or name and get data
-  //pass data into api nd get back data
-  //dend that data as a render obj
-  res.render('spaces/show', {images, cloudinary});
-});
-
-
-
-/////// THIS IS DONE DONT TOUCH IT ///////
-router.get('/new', isLoggedIn, function(req, res) {
-  res.render('spaces/new');
-});
 
 /////// THIS IS DONE DONT TOUCH IT ///////
 router.post('/', upload.single('myImage'), function(req, res){
@@ -53,6 +45,29 @@ router.post('/', upload.single('myImage'), function(req, res){
         });
       });
     res.redirect('/');
+  });
+});
+
+
+/////// THIS IS DONE DONT TOUCH IT ///////
+router.get('/new', isLoggedIn, function(req, res) {
+  res.render('spaces/new');
+});
+
+
+///// WORK ON THIS NEXT /////
+router.get('/:name', isLoggedIn, function(req, res) {
+  db.space.findOne({
+    where: {
+      userId: req.user.id,
+      name: req.params.name
+     },
+  }).then(function(space){
+    var spaceUrl = space.url;
+    var url = spaceUrl.substring(8);
+    console.log(url);
+    //pass data into api and get back data
+    res.render('spaces/show', {space: space});
   });
 });
 
